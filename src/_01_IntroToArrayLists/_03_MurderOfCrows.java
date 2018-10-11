0137d0fbcd0ef1540efafffd71f0fc5c2b99caba
11000783 <josephaguilar2022@gmail.com>
2018-10-10 18:29:20 -0700

    guestbook, murder of crows IP

--- src/_01_IntroToArrayLists/_03_MurderOfCrows.java
+++ src/_01_IntroToArrayLists/_03_MurderOfCrows.java
@@ -30,7 +30,10 @@ public class _03_MurderOfCrows {
          * 1. One of the Crows has eaten the diamond. You need to search through the stomach of each Crow, 
          * then print the name of the guilty Crow.
          */
-        
+      for(Crow c:theMurder) {
+    	  c.getStomachContents();
+    	
+      }
         /* 2. How many innocent crows had to die before the diamond was found? */
     	
     }
